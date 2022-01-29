4fdadd660e10e5c9fa64dc3296d88b671e18dcbe
leena <annachandy123@gmail.com>
2022-01-30 10:45:25 +1100

    loginform added

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 // Import React Router Link component for internal hyperlinks
 import { Link } from "react-router-dom";
-
+import Auth from "../../utils/auth";
 const Header = () => {
+    const logout = (event) => {
+        event.preventDefault();
+        Auth.logout();
+    };
     return (
         <header className="bg-info text-dark mb-4 py-3 display-flex align-center">
             <div className="container flex-column justify-space-between-lg justify-center align-center text-center">
@@ -15,6 +19,27 @@ const Header = () => {
                 <p className="m-0" style={{ fontSize: "1.75rem", fontWeight: "700" }}>
                     Manage Site Delivery.
                 </p>
+                <div>
+                    {Auth.loggedIn() ? (
+                        <>
+                            <Link className="btn btn-lg btn-info m-2" to="/me">
+                                {Auth.getProfile().data.username}'s profile
+                            </Link>
+                            <button className="btn btn-lg btn-light m-2" onClick={logout}>
+                                Logout
+                            </button>
+                        </>
+                    ) : (
+                        <>
+                            <Link className="btn btn-lg btn-info m-2" to="/login">
+                                Login
+                            </Link>
+                            <Link className="btn btn-lg btn-light m-2" to="/signup">
+                                Signup
+                            </Link>
+                        </>
+                    )}
+                </div>
             </div>
         </header>
     );
