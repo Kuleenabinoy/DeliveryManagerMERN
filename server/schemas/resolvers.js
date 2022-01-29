d56248c7e0b1a7ce843637ef5febeee72794e1eb
leena <annachandy123@gmail.com>
2022-01-29 18:00:49 +1100

    usertypedef added

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,6 @@
-const { Team } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { Team, User } = require("../models");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
     Query: {
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -9,13 +9,30 @@ const typeDefs = gql`
         createdAt: String
         items: [String]!
     }
-
+    type User {
+        _id: ID
+        username: String
+        email: String
+        employeeCategory: String
+        password: String
+        teams: [Team]!
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
     type Query {
+        users: [User]
+        user(username: String!, employeeCategory: String!): User
+        teams(username: String): Team
         teams: [Team]!
         team(teamId: ID!): Team
+        me: User
     }
 
     type Mutation {
+        addUser(username: String!, email: String!, password: String!, employeeCategory: String!): Auth
+        login(email: String!, password: String!): Auth
         addTeam(name: String!, siteInfo: String!, email: String!): Team
         addItem(teamId: ID!, item: String!): Team
         removeTeam(teamId: ID!): Team
