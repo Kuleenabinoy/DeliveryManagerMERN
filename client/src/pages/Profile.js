4bde7c30365071cc6e82fa99b616e4eb746e6e71
leena <annachandy123@gmail.com>
2022-02-02 18:52:36 +1100

    randomNo

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,51 +1,51 @@
-import React from "react";
+// import React from "react";
 
-import { Redirect, useParams } from "react-router-dom";
-import { useQuery } from "@apollo/client";
+// import { Redirect, useParams } from "react-router-dom";
+// import { useQuery } from "@apollo/client";
 
-//import ProfileForm from "../components/ItemsList";
-import ProfileList from "../components/ItemForm";
+// //import ProfileForm from "../components/ItemsList";
+// import ProfileList from "../components/ItemForm";
 
-import { QUERY_USER, QUERY_ME } from "../utils/queries";
+// import { QUERY_USER, QUERY_ME } from "../utils/queries";
 
-import Auth from "../utils/auth";
+// import Auth from "../utils/auth";
 
-const Profile = () => {
-    const { username: userParam } = useParams();
+// const Profile = () => {
+//     const { username: userParam } = useParams();
 
-    // If there is no `profileId` in the URL as a parameter, execute the `QUERY_ME` query instead for the logged in user's information
-    const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
-        variables: { username: userParam },
-    });
+//     // If there is no `profileId` in the URL as a parameter, execute the `QUERY_ME` query instead for the logged in user's information
+//     const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+//         variables: { username: userParam },
+//     });
 
-    // Check if data is returning from the `QUERY_ME` query, then the `QUERY_SINGLE_PROFILE` query
-    const user = data?.me || data?.user || {};
-    console.log(user);
-    // Use React Router's `<Redirect />` component to redirect to personal profile page if username is yours
-    if (Auth.loggedIn() && Auth.getProfile().data.username) {
-        return <Redirect to="/me" />;
-    }
-    console.log(data.user.username, "usename");
+//     // Check if data is returning from the `QUERY_ME` query, then the `QUERY_SINGLE_PROFILE` query
+//     const user = data?.me || data?.user || {};
+//     console.log(user);
+//     // Use React Router's `<Redirect />` component to redirect to personal profile page if username is yours
+//     if (Auth.loggedIn() && Auth.getProfile().data.username) {
+//         return <Redirect to="/me" />;
+//     }
+//     console.log(data.user.username, "usename");
 
-    if (loading) {
-        return <div>Loading...</div>;
-    }
+//     if (loading) {
+//         return <div>Loading...</div>;
+//     }
 
-    if (!user?.username) {
-        return <h4>You need to be logged in to see your page. Use the navigation links above to sign up or log in!</h4>;
-    }
+//     if (!user?.username) {
+//         return <h4>You need to be logged in to see your page. Use the navigation links above to sign up or log in!</h4>;
+//     }
 
-    return (
-        <div>
-            <div>
-                <h2 className="card-header">Deliver Note created At {user.createdAt}</h2>
-                <h2 className="card-header">Deliver to {user.siteInfo}</h2>
-                <h2 className="card-header">{user.name}'s items to pick from warehouse...</h2>
+//     return (
+//         <div>
+//             <div>
+//                 <h2 className="card-header">Deliver Note created At {user.createdAt}</h2>
+//                 <h2 className="card-header">Deliver to {user.siteInfo}</h2>
+//                 <h2 className="card-header">{user.name}'s items to pick from warehouse...</h2>
 
-                {user.items?.length > 0 && <ProfileList items={user.items} />}
-            </div>
-        </div>
-    );
-};
+//                 {user.items?.length > 0 && <ProfileList items={user.items} />}
+//             </div>
+//         </div>
+//     );
+// };
 
-export default Profile;
+// export default Profile;
