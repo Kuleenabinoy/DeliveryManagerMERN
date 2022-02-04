a38c96344ff13a607edddd3b09701db5770ec018
leena <annachandy123@gmail.com>
2022-02-04 16:34:50 +1100

    employee profileteam not getting rendering

--- client/src/pages/Employee.js
+++ client/src/pages/Employee.js
@@ -6,30 +6,17 @@ import { useQuery } from "@apollo/client";
 import { QUERY_USER, QUERY_ME } from "../utils/queries";
 
 import Auth from "../utils/auth";
-
+//team is not getting rendering
 const Employee = () => {
     const { username: userParam } = useParams();
     const user = Auth.getProfile().data;
 
-    // If there is no `profileId` in the URL as a parameter, execute the `QUERY_ME` query instead for the logged in user's information
     const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
         variables: { username: user.username },
     });
 
-    debugger;
     console.log(user, "user");
 
-    // console.log("userName", userParam);
-    // Check if data is returning from the `QUERY_ME` query, then the `QUERY_SINGLE_PROFILE` query
-    //const user = data?.me || data?.user || {};
-    //console.log("user in employee page", Auth.getProfile().data);
-
-    // Use React Router's `<Redirect />` component to redirect to personal profile page if username is yours
-    // if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
-    //     return <Redirect to="/me" />;
-    // }
-    // const user1 = data?.me || data?.user || {};
-    // console.log("user1", user);
     console.log("data", data);
 
     if (loading) {
@@ -50,16 +37,7 @@ const Employee = () => {
                 <h4>DeliveryOrder Created At</h4>
                 <h4>Site Address</h4>
                 <h4>Items</h4>
-                {/* <h2>You have delivery to {siteInfo}site</h2> */}
-                {/* <ItemsList user={user.username} title={`${user.username}'s items`} /> */}
-                {/* <ProfileForm /> */}
             </div>
-
-            {/* <h2 className="card-header">Deliver Note created At {data.createdAt}</h2>
-                <h2 className="card-header">Deliver to {team.siteInfo}</h2>
-                <h2 className="card-header">{team.name}'s items to pick from warehouse...</h2> */}
-            {/* <ProfileForm siteinfo={user.siteinfo} /> */}
-            {/* <ProfileForm siteinfo={user.sitenfo} items={user.items} /> */}
         </div>
     );
 };
