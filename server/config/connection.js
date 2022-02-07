cc6cef8a76ec4066ce23391d839e123a03661589
leena <annachandy123@gmail.com>
2022-02-07 19:41:20 +1100

    test

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Delivery-Team", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-});
+mongoose.connect(
+    "mongodb+srv://kuleena:rootroot@cluster0.xsrir.mongodb.net/Delivery-Team?retryWrites=true&w=majority" ||
+        "mongodb://localhost/Delivery-Team",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+    }
+);
 
 module.exports = mongoose.connection;
