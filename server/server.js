f805df131fd244efebd90386d3b8f4f066012f81
leena <annachandy123@gmail.com>
2022-02-08 10:00:53 +1100

    make it better

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const { authMiddleware } = require("./utils/auth");
 const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
+//
 const app = express();
 
 const server = new ApolloServer({
